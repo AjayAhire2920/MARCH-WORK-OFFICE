0bee6f0a6fc3b9ac1a8f11b970875b9d374edafa
Ajay Ahire <ajay.mojoestech@gmail.com>
2021-03-23 09:47:42 +0530

    created admin panel

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $ : any;
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
@@ -10,6 +10,25 @@ export class AdminPanelComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    function handleSidebarActiveClass() {
+      // if we open in tablet devices
+      //sidebar will have active class
+      if ($(window).width() >= 768) {
+        $("#sidebar").addClass("active");
+      } else {
+        $("#sidebar").removeClass("active");
+      }
+    }
+    handleSidebarActiveClass();
+    // resize eventlistener
+    $(window).resize(() => {
+      handleSidebarActiveClass();
+    });
+    $(document).ready(function () {
+      $("#sidebarCollapse").on("click", function () {
+        $("#sidebar").toggleClass("active");
+      });
+    });
   }
 
 }
